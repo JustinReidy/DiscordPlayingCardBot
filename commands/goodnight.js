f26a5c39f8f6d1c5a3b052d955a93155998eeca8
Justin Reidy <justintreidy@gmail.com>
2021-02-15 18:28:24 -0600

    Added check to uwu.js to see if a argument was passed, if not only sends 1 message

--- commands/goodnight.js
+++ commands/goodnight.js
@@ -2,6 +2,7 @@ module.exports = {
     name: "goodnight",
     aliases: ["gn"],
     execute(message, args){
-        message.channel.send("Bot shutting down, ask Justin to restart it. Goodnight, ya dirty pair of cum stains. ")
+	let valid = "No"
+        message.channel.send(`Bot shutting down, ask Justin to restart it.\n Does this command actually do anything? ${valid}`)
     }
 }
---
+++ commands/goodnight.js~
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "goodnight",
+    aliases: ["gn"],
+    execute(message, args){
+        message.channel.send("Bot shutting down, ask Justin to restart it. Goodnight, ya dirty pair of cum stains. ")
+    }
+}
